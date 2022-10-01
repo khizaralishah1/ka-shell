d08438cefa23fbf15630c130910144f8f6d43d40
khizaralishahcs <khizaralishahcs07@gmail.com>
2022-10-01 13:28:59 +0500

    working, 1 oct

--- kash.c
+++ kash.c
@@ -3,8 +3,10 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#define LEN 10
+#define LEN 15
 #define CMD_NAME_LEN 10
+#define MAX_ARGS 10
+#define ARG_LEN 10
 
 typedef struct cmd cmd;
 struct cmd {
@@ -20,13 +22,56 @@ char* get_cmd_name(char* buf)
 
     for(size_t i = 0; i < CMD_NAME_LEN-1; ++i)
     {
-        if(buf[i] != ' ')
+        if(buf[i] != ' ' && buf[i] != '\n')
             cmd_name[i] = buf[i];
         else
             break;
     }
 
+    printf("cmd_name = %s\n", cmd_name);
+
     return cmd_name;
+}
+
+char** cmd_get_args(char* buf)
+{
+    char** argv = NULL;
+    size_t i = 0;
+
+    for(; i < MAX_ARGS; ++i)
+    {
+        if(*buf == '\0' || *buf == '\n')
+        {
+            argv[i] = (char*) malloc( sizeof(char) * ARG_LEN );
+            argv[i] = NULL;
+            return argv;
+        }
+
+        argv[i] = (char*) malloc( sizeof(char) * ARG_LEN );
+        memset(argv[i], 0, ARG_LEN);
+
+        for(int k=0; k<ARG_LEN; ++k)
+        {
+            if(*buf == '\0')
+            {
+                argv[i] = (char*) malloc( sizeof(char) * ARG_LEN );
+                argv[i] = NULL;
+                return argv;
+            }
+            if(*buf != ' ') argv[i][k] = *buf;
+            else
+            {
+                while(*buf == ' ') ++buf;
+                break;
+            }
+            ++buf;
+        }
+    }
+
+    argv[i] = (char*) malloc( sizeof(char) * ARG_LEN );
+    argv[i] = NULL;
+
+    return argv;
 }
 
 char* cmd_input(char* buf)
@@ -43,65 +88,67 @@ char* cmd_input(char* buf)
     }
 
     return buf;
-};
+}
 
-void cmd_parse(char* buf)
+void cmd_parse_run(char* buf)
 {
-    while(*buf == ' ') ++buf; // removes initial whitespaces
+    while(*buf == ' ' && *buf != '\0') ++buf; // removes initial whitespaces
+
+    if(*buf == '\0')
+        exit(EXIT_SUCCESS);
 
     //extract the cmd name
     char* cmd_name = get_cmd_name(buf);
 
+    for(int i=0; i<LEN; ++i)
+    {
+        printf("buf[%d] = %c\n", i, buf[i]);
+    }
+
+    //extract the arguments
+    while(*buf != ' ' && *buf != '\0') ++buf; // go through the cmd_name
+    while(*buf == ' ' && *buf != '\0') ++buf; // go through spaces after it
+
+    char** arg_var = NULL;
+    
+    if(*buf != '\0')
+        arg_var = cmd_get_args(buf); // the first character of first argument is inclusive
+
+    for(int i=0; i<MAX_ARGS; ++i)
+    {
+        printf("arg[%d] = %s\n", i, arg_var[i]);
+    }
+    
     int pid = fork();
 
     if(pid == 0)
-        execvp(cmd_name, NULL);
+    {
+        execvp(cmd_name, arg_var);
+    }
     else
     {
-        wait(NULL);
+        wait(&pid);
         free(cmd_name);
-    }     
-};
+        free(arg_var);
+    }
+}
 
 int main()
 {
-    
-    // printf("a\n");
-    // for(int i=0; i<LEN; ++i)
-    // {
-    //     printf("%p = %c\n", &buff[i], buff[i]);
-    // }
     char * buff = (char *)malloc(LEN * sizeof(char)); // buffer for storing user input
+    memset(buff, 0, LEN * sizeof(char));
     char * const old_buff = buff;
-    while(strcmp(buff))
+
+    while(1)
     {
         memset(buff, 0, LEN * sizeof(char)); // puts all 0s in the mem location from address stored in buf to buf+LEN
         printf("kash>");
         cmd_input(buff);
-        cmd_parse(buff);
+        cmd_parse_run(buff);
         buff = old_buff;
     }
     
     free(buff);
-
-    // for(int i=0; i<LEN; ++i)
-    // {
-    //     printf("%p = %c\n", &buff[i], buff[i]);
-    // }
-
-    // for(int i=0; i<LEN; ++i)
-    // {
-    //     if(buff[i] == ' ')
-    //         printf("spaces: %i\n", i);
-    //     else break;
-    // }
-
-    // printf("second\n");
-    
-    // for(int i=0; i<LEN; ++i)
-    // {
-    //     printf("%p = %c\n", &buf2[i], buf2[i]);
-    // }
 
     return EXIT_SUCCESS;
 }
